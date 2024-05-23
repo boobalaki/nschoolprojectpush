1854cf1787a82ec43b17278371ce56f3faaf19b9
boobalaki <boobalakiruthra@gmail.com>
2024-05-23 13:02:06 +0530

    nschool project

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./App.css";
+import Ios_training from "./components/Ios";
+import FullStackComponent from "./components/fullstack";
+import ReactComponent from "./components/react";
+import AngularComponent from "./components/angular";
+import NodeJsComponent from "./components/nodejs";
+import FlutterComponent from "./components/flutter";
+import ReactnativeComponent from "./components/Reactnatives";
+import AndroidComponent from "./components/android";
+import PhpComponent from "./components/php";
+import WebdesignComponent from "./components/webdesign";
+import PythonComponent from "./components/python";
+import DigitalComponent from "./components/digital";
+import MongodbComponent from "./components/mongodb";
+import CorejavaComponent from "./components/corejava";
+import SoftwaretestComponent from "./components/softwaretest";
+import MernstackComponent from "./components/mernstack";
+import MeanstackComponent from "./components/meanstack";
+import Machine_learningComponent from "./components/machine_learning";
+import Dev_opsComponent from "./components/Devops";
+import BigdataComponent from "./components/Bigdata";
+import DatascienceComponent from "./components/datascience";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <FullStackComponent />
+        {/* <ReactComponent /> */}
+        {/* <AngularComponent /> */}
+        {/* <NodeJsComponent /> */}
+        {/* <Ios_training />    */}
+          {/* <ReactnativeComponent/> */}
+        {/* <FlutterComponent/> */}
+        {/* <AndroidComponent/> */}
+      {/* <PhpComponent/> */}
+      {/* <WebdesignComponent/> */}
+      {/* <PythonComponent/> */}
+      {/* <DigitalComponent/> */}
+      {/* <MongodbComponent/> */}
+      {/* <CorejavaComponent/> */}
+      {/* <SoftwaretestComponent/> */}
+      {/* <MernstackComponent/> */}
+      {/* <MeanstackComponent/> */}
+     {/* <Machine_learningComponent/> */}
+     {/* <Dev_opsComponent/> */}
+     {/* <BigdataComponent/> */}
+     {/* <DatascienceComponent/> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
